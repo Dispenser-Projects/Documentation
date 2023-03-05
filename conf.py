649b0b2350079407d2adfb24d7a48f17fbff15c3
Vincent (Leirof) <vince.lrf@gmail.com>
2023-03-05 16:09:07 +0100

    📝 Added support button

--- conf.py
+++ conf.py
@@ -38,6 +38,11 @@ html_theme_options = {
     },
     "icon_links": [
         {
+            "name": "Support us",
+            "url": "https://utip.io/gunivers",
+            "icon": "fa fa-heart",
+        },
+        {
             "name": "Gunivers",
             "url": "https://gunivers.net",
             "icon": "_static/logo-gunivers.png",
